c899045600d9688fe7e6164f1ea9e298d43385e0
Tom Jardine-McNamara <tom@itsravenous.com>
2020-08-22 19:43:26 +0100

    initial commit

---
+++ functions/inventory.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// ------------------------
+// Bolt App Initialization
+// ------------------------
+const { App, ExpressReceiver } = require('@slack/bolt');
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  processBeforeResponse: true,
+});
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: expressReceiver,
+  processBeforeResponse: true,
+});
+
+// ------------------------
+// Application Logic
+// ------------------------
+
+app.command('/echo', async ({ command, logger, ack, say }) => {
+  try {
+    await say(`${command.text}`);
+    await ack();
+  } catch (e) {
+    logger.error(e);
+    await ack(`:x: Failed to post a message (error: ${e})`);
+  }
+});
+
+// ------------------------
+// AWS Lambda handler
+// ------------------------
+const awsServerlessExpress = require('aws-serverless-express');
+const server = awsServerlessExpress.createServer(expressReceiver.app);
+module.exports.app = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context);
+}
